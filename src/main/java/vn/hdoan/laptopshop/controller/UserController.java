42a41c4448c58882165b9c33b8bcf196a96ef736
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-08-21 04:06:25 +0700

    send,get data with html form in MVC

--- src/main/java/vn/hdoan/laptopshop/controller/UserController.java
+++ src/main/java/vn/hdoan/laptopshop/controller/UserController.java
@@ -2,9 +2,8 @@ package vn.hdoan.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import vn.hdoan.laptopshop.domain.User;
 import vn.hdoan.laptopshop.service.UserService;
 
 @Controller
@@ -25,22 +24,18 @@ public class UserController {
         return "hello";
     }
 
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println("run here" + hoidanit);
+        return "hello";
+    }
 }
 
-//@RestController
-//public class UserController {
-//
-//    // DI: dependency injection
-//    private UserService userService;
-//
-//    public UserController(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @GetMapping("/")
-//    public String getHomePage(){
-//        return this.userService.handleHello();
-//    }
-//
-//}
 
--- src/main/webapp/WEB-INF/view/hello.jsp
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -8,16 +8,29 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-</head>
+
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled JavaScript -->
+    <cript src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></cript>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+<%--    <link rel="stylesheet" href="/css/demo.css">--%>
+
+
+    </head>
 <body>
     hello from jsp
     <h1>
+        <c:out value="${eric}" />
         ${eric}
     </h1>
 
-    <h1>
+    <h2>
         ${hoidanit}
-    </h1>
+    </h2>
 
+    <button class="btn btn-warning">Submit</button>
 </body>
 </html>
