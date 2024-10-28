0c62a7b8e7e831bc0b2bc83b0e9584de82643ed0
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-10-28 22:16:28 +0700

    view detail product done

--- src/main/java/vn/hdoan/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hdoan/laptopshop/controller/client/ItemController.java
@@ -4,12 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import vn.hdoan.laptopshop.domain.Product;
+import vn.hdoan.laptopshop.service.ProductService;
 
 @Controller
 public class ItemController {
 
+    private final ProductService productService;
+
+    public ItemController(ProductService productService){
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
     public String getProductPage(Model model, @PathVariable long id){
+        Product pr = this.productService.fetchProductById(id).get();
+        model.addAttribute("product", pr);
+        model.addAttribute("id", id);
         return "/client/product/detail";
     }
 }
