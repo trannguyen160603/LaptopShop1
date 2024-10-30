737dee0cac290b78fd106d1069de1682196aa697
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-10-30 11:51:31 +0700

    loadUserByUsername feature

--- src/main/java/vn/hdoan/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hdoan/laptopshop/LaptopshopApplication.java
@@ -3,8 +3,8 @@ package vn.hdoan.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+@SpringBootApplication
+//@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/vn/hdoan/laptopshop/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package vn.hdoan.laptopshop.service;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // logic
+        vn.hdoan.laptopshop.domain.User user = this.userService.getUserByEmail(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+                );
+
+    }
+}
